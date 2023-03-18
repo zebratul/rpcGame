3a0fb205bafee5057ba7df094c7b41511e02e427
zebratul <zebratul@gmail.com>
2023-03-18 19:06:18 +0300

    Update rpc.js
    Updated functionality, small refactor

--- rpc.js
+++ rpc.js
@@ -4,65 +4,67 @@ const readline = require('readline');
 class Game {
     constructor(moves) {
         this.moves = moves;
-        // this.table = new Table(moves);
-        this.winner = new Winner(moves);
-        this.keyGenerator = new CryptoUtils();
-        this.start();
-      }
-  
-    start() {
-        if (moves.length % 2 === 0 || moves.length < 3 || new Set(moves).size !== moves.length) {
-            console.log('Error: please provide an odd number of non-repeating moves (minimum 3).');
-            console.log('Example usage: node index.js rock paper scissors');
-        return;
-        }
-        const computerMove = this.moves[Math.floor(Math.random() * this.moves.length)];
-    //   const key = this.keyGenerator.generateKey();
-    //   const hmac = crypto.createHmac("sha3-256", key);
-    //   hmac.update(computerMove);
-        console.log(`** Welcome to the game. **`);
-        console.log(`Computer has chosen it's move. HMAC: TBD `);
-        console.log(`Available moves:`);
-        moves.forEach((element, index) => console.log(`${index+1}: ${element}`));
-
-        const rl = readline.createInterface({
+        this.table = new Table(moves);
+        this.winner = new WinnerSelector(moves);
+        this.cryptoUtils = new CryptoUtils();
+        this.key = this.cryptoUtils.generateSecureKey();
+        this.computerMove = this.moves[crypto.randomInt(this.moves.length)];
+        this.rl = readline.createInterface({
             input: process.stdin,
             output: process.stdout,
         });
+        this.start();
+    }
   
-        rl.question(`Please, enter your move ["0" to exit, "help" for moves info]: `, (answer) => {
-            if (answer === "0") {
-                console.log("Exiting the game...");
-                rl.close();
-        } else {
-            const userMove = this.moves[answer - 1];
-            if (!userMove) {
-                console.log(`Invalid move: ${answer}. Please try again.`);
-                this.start();
-            } else {
-                console.log(`Your move: ${userMove}`);
-                console.log(`Computer's move: ${computerMove}`);
-                const result = this.winner.determineWinner(userMove, computerMove);
-                if (result === "draw") {
-                    console.log("It's a draw!");
-                } else if (result === "user") {
-                    console.log("You win!");
-                } else {
-                    console.log("You lose!");
-                }
-                console.log(`HMAC key: tbd`);
-                rl.close();
-            }
+    start() {
+        if (this.moves.length % 2 === 0 || this.moves.length < 3 || new Set(this.moves).size !== this.moves.length) {
+            console.log('Error: please provide an odd number of non-repeating moves (minimum 3).\nExample usage: node index.js rock paper scissors');
+            return;
         }
-      });
+        const hmac = this.cryptoUtils.generateHmac(this.key, this.computerMove);
+        console.log(`\n** Welcome to the game. **\nComputer has chosen its move. HMAC: ${hmac}\nAvailable moves:`);
+        this.moves.forEach((element, index) => console.log(`${index+1}: ${element}`));
+        this.readUserInput();
+    }
+  
+    readUserInput() {
+        this.rl.question(`Please enter your move [Type a number to choose your move, "0" to exit, "help" for moves info]: `, (answer) => {
+            if (answer === "0") {
+            console.log("Exiting the game...\n");
+            this.rl.close();
+            } else {
+            this.showResults(answer);
+            }
+        });
+    }
+  
+    showResults(answer) {
+        const userMove = this.moves[answer - 1];
+        if (!userMove) {
+            console.log(`\nInvalid move: ${answer}. Please try again.`);
+            this.start();
+        } else {
+            console.log(`\n**Your move: ${userMove}**`);
+            console.log(`Computer's move: ${this.computerMove}`);
+            const result = this.winner.determineWinner(userMove, this.computerMove);
+            if (result === "draw") {
+            console.log("It's a draw!");
+            } else if (result === "user") {
+            console.log("You win!");
+            } else {
+            console.log("You lose!");
+            }
+            console.log(`HMAC key: ${this.key}\n`);
+            this.rl.close();
+        }
     }
   }
   
-  class Winner {
+class WinnerSelector {
       constructor(moves) {
           this.moves = moves;
     }
-  
+
     determineWinner(userMove, computerMove) {
         const index = this.moves.indexOf(userMove);
         const half = Math.ceil(this.moves.length / 2);
@@ -78,14 +80,20 @@ class Game {
   }
   
 class CryptoUtils {
-    static generateSecureKey() {
+    generateSecureKey() {
         return crypto.randomBytes(32).toString('hex');
     }
-
-    static generateHmac(key, data) {
+    
+    generateHmac(key, data) {
         const hmac = crypto.createHmac('sha3-256', key);
         hmac.update(data);
         return hmac.digest('hex');
+    }
+  }
+
+class Table {
+          constructor(moves) {
+          this.moves = moves;
     }
 }
 
